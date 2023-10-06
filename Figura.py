6fbfcf9ff58cea23f4865bb740eb8a055f4eb9f5
vitin18 <114629596+vitin18@users.noreply.github.com>
2023-10-06 10:18:35 -0300

    Update Figura.py

--- Figura.py
+++ Figura.py
@@ -1,11 +1,13 @@
 import math
 import time
 
+
 def animacao(texto):
     for char in texto:
         print(char, end='', flush=True)
-        time.sleep(0.03)
+        time.sleep(0.01)
     print()
+
 
 class Figura:
     def __init__(self, tipo):
@@ -25,6 +27,7 @@ class Figura:
     def calcular_momento_inercia(self):
         pass
 
+
 class QuadradoRetangulo(Figura):
     def __init__(self, base, altura):
         super().__init__("Quadrado/Retângulo")
@@ -35,12 +38,13 @@ class QuadradoRetangulo(Figura):
         self.area = self.base * self.altura
 
     def calcular_centro_gravidade(self):
-        self.centro_gx = self.base / 2
-        self.centro_gy = self.altura / 2
+        self.centro_gx = (self.base / 2) * self.area
+        self.centro_gy = (self.altura / 2) * self.area
 
     def calcular_momento_inercia(self):
         self.momento_inercia_x = (self.altura ** 3) * self.base / 12
         self.momento_inercia_y = (self.base ** 3) * self.altura / 12
+
 
 class Triangulo(Figura):
     def __init__(self, base, altura):
@@ -52,12 +56,13 @@ class Triangulo(Figura):
         self.area = (self.base * self.altura) / 2
 
     def calcular_centro_gravidade(self):
-        self.centro_gx = self.base / 3
-        self.centro_gy = self.altura / 3
+        self.centro_gx = (self.base / 3) * self.area
+        self.centro_gy = (self.altura / 3) * self.area
 
     def calcular_momento_inercia(self):
         self.momento_inercia_x = (self.altura ** 3) * self.base / 36
         self.momento_inercia_y = (self.base ** 3) * self.altura / 36
+
 
 class Circulo(Figura):
     def __init__(self, raio):
@@ -68,33 +73,39 @@ class Circulo(Figura):
         self.area = math.pi * (self.raio ** 2)
 
     def calcular_centro_gravidade(self):
-        self.centro_gx = 0
-        self.centro_gy = 0
+        self.centro_gx = self.raio / 2
+        self.centro_gy = self.raio / 2
 
     def calcular_momento_inercia(self):
         self.momento_inercia_x = (math.pi * self.raio ** 4) / 4
         self.momento_inercia_y = (math.pi * self.raio ** 4) / 4
 
-class Semicirculo(Figura):
+
+class um_quarto_circulo(Figura):
     def __init__(self, raio, direcao):
-        super().__init__("Semicírculo")
+        super().__init__("Um quarto de Círculo")
         self.raio = raio
         self.direcao = direcao
 
     def calcular_area(self):
-        self.area = (math.pi * (self.raio ** 2)) / 2
+        self.area = (math.pi * (self.raio ** 2)) / 4
 
     def calcular_centro_gravidade(self):
         if self.direcao in [1, 3]:
-            self.centro_gx = 0
-            self.centro_gy = 0
+            self.centro_gx = self.raio / 8
+            self.centro_gy = self.raio / 8
         else:
-            self.centro_gx = 0
-            self.centro_gy = (4 * self.raio / (3 * math.pi))
+            self.centro_gx = self.raio / 2
+            self.centro_gy = self.raio / 2
 
     def calcular_momento_inercia(self):
-        self.momento_inercia_x = (math.pi * self.raio ** 4) / 4
-        self.momento_inercia_y = (math.pi * self.raio ** 4) / 4
+        if self.direcao in [1, 3]:
+            self.momento_inercia_x = (math.pi * self.raio ** 4) / 32
+            self.momento_inercia_y = (math.pi * self.raio ** 4) / 32
+        else:
+            self.momento_inercia_x = (math.pi * self.raio ** 4) / 8
+            self.momento_inercia_y = (math.pi * self.raio ** 4) / 8
+
 
 def obter_coordenadas(mensagem):
     while True:
@@ -107,3 +118,25 @@ def obter_coordenadas(mensagem):
             except ValueError:
                 pass
         print("Por favor, digite as coordenadas corretamente (formato: x,y).")
+
+
+class Semicirculo(Figura):
+    def __init__(self, raio, direcao):
+        super().__init__("Semicírculo")
+        self.raio = raio
+        self.direcao = direcao
+
+    def calcular_area(self):
+        self.area = (math.pi * (self.raio ** 2)) / 2
+
+    def calcular_centro_gravidade(self):
+        if self.direcao in [1, 3]:
+            self.centro_gx = self.raio / 4
+            self.centro_gy = self.raio / 4
+        else:
+            self.centro_gx = 0
+            self.centro_gy = (4 * self.raio / (3 * math.pi))
+
+    def calcular_momento_inercia(self):
+        self.momento_inercia_x = (math.pi * self.raio ** 4) / 8
+        self.momento_inercia_y = (math.pi * self.raio ** 4) / 8
